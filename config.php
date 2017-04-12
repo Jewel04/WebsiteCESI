3eb3f818c98d0f9c9c9f121b26e307746c23570f
Jewel04 <noeline.wyart@viacesi.fr>
2017-04-12 15:26:21 +0200

    First test + singleton ( BDD con)

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Noëline
+ * Date: 12/04/2017
+ * Time: 15:24
+ */
+require_once 'singleton.php';
+
+singleton::setConfig('mysql:host=localhost;dbname=users;charset=latin1', 'root', '');
